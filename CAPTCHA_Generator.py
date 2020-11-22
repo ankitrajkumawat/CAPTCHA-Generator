2c505ca7940ef93fbb1936ae22a766010b9bc420
ankitrajkumawat <71167922+ankitrajkumawat@users.noreply.github.com>
2020-11-22 10:35:17 +0530

    Add files via upload

--- CAPTCHA_Generator.py
+++ CAPTCHA_Generator.py
@@ -3,7 +3,6 @@ import random
 
 s=''.join(random.choice(string.ascii_uppercase+ string.ascii_lowercase + string.digits) for i in range(6))
 print(s)
-NodeKey="A6yHZl6a"
 user_input=input("Enter The Captcha displayed :: ")
 if(user_input==s):
     print("Successful Login")
